9767523ee6f3dc0cb94407f04f0a4b1eb24f95f7
abanoob <abanoobsamy900@gmail.com>
2026-03-15 20:47:17 +0200

    Initial Commit

---
+++ RecipeStreamTests/RecipeStreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeStreamTests.swift
+//  RecipeStreamTests
+//
+//  Created by Abanoob Samy on 15/03/2026.
+//
+
+import Testing
+@testable import RecipeStream
+
+struct RecipeStreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
